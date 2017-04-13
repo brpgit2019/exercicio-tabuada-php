f2483b275cd323a37681230184be571f7bcb35b5
brpgit <bruno2144@gmail.comgit config --global user.email bruno2144@gmail.com>
2017-04-13 16:17:27 -0300

    Exercicio Tabuada

---
+++ minha-tabuada-em-php.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<title>Minha Tabuada em PHP</title>
+	<link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+	<form method="post" action="resultado.php">
+		<div id="caixa">
+			<P>Digite um numero:</P>
+			<input type="text" name="valor" id="valor" /><p>
+			<input type="submit" name="cacule" value="Calcule" />
+			<input type="reset" name="limpar" value="Limpar">
+		</div>
+	</form>
+</body>
+</html>
---
+++ resultado.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<title>Resultado</title>
+	<link rel="stylesheet" href="resultado.css" />
+</head>
+<body>
+	<div id="php">
+		<?php
+			if ($_POST['valor']!=""){
+			$v1= $_POST['valor'];
+			$acumladora = 0;
+			for ($n=0; $n<11; $n++){
+			$res = $n*$v1;
+			$acumladora = $acumladora +$res;
+			echo $v1." x ".$n." = ".$res."<p>";
+			}
+			echo " A soma de todos os resultado é! ".$acumladora;
+			}
+		?>
+	</div>
+</body>
+</html>
